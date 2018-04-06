a9564b128a8a0c96bfc4241b818239d35ad5a868
nomin5511 <namgalan@uci.edu>
2018-04-06 16:48:27 -0700

    Update CAH_deck.py

--- CAH_deck.py
+++ CAH_deck.py
@@ -1,22 +1,7 @@
-CAH_B_DECK = {"In the end, Mei's grandfather died due to ______",
-                  "______ and ______ are now step sisters?",
-                  "Why did Aihara Yuzu get expelled?",
-                  "Other than bears, Mei loved ______",
-                  "Nene's crackship is ______ and ______",
-                  "Yuzu spent all week working on the perfect date that ended with ______",
-                  "In secret ______ was jealous of Nina's boobs",
-                  "The most unexpected obstacle to get in the way of YuzuMei was ______",
-                  "______'s catchphrase is ______",
-                  "_______ resulted in supplementary school of ______"
-                  }
-    
-CAH_B_DECK_NSFW = {"______ wrote smutty fanfic about _______ and ______" ,
-                   "The best thing ______ learned from Peach Sisters was ______",
-                   "______ is the true top and ______ is the true bottom"}
+''' File for custom black and white cards '''
 
-CAH_W_DECK = {"The Gayness", "The strict rules of Aihara family", 
-                  "Chapter 36", "Matsuri", "Yuzu", "Mei",
-                  "Harumi", "Himeko", "Sho", "Kumagorou", "Grandfather", "Ume",
-                  "Manager", "Sabutroll", "Himeko's dog army", "Peach Sisters", "Chairman's Office", 
-                  }
-CAH_W_DECK_NSFW = {"Scisorring", "Handholding"}
+
+CAH_B_DECK = set()   
+CAH_B_DECK_NSFW = set()
+CAH_W_DECK = set()
+CAH_W_DECK_NSFW = set()
